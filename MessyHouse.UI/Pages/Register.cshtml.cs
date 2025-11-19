ab3d624aacaaa40a61631ad5576b49d8455bdb25
krishveniSequoiaat <krisshveni@sequoiaat.com>
2025-11-19 11:46:19 +0530

    Changes on Ui for User  authentication

---
+++ MessyHouse.UI/Pages/Register.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MessyHouse.UI.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
